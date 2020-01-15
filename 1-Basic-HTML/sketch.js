2a646e8cae47f072d192af48d4acfc67a9fc20d1
Jeffrey <Jeffreym0215@gmail.com>
2020-01-15 14:06:21 -0800

    This is a rough ROUGH draft of our website

--- 1-Basic-HTML/sketch.js
+++ 1-Basic-HTML/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
+  canvas = createCanvas(700,700);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
+
+
   // put setup code here
 }
 
 function draw() {
-  // put drawing code here
+background("red");
 }
