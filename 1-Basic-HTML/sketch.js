01902cdf1973134395474654bcbdcb8c87a3111e
Jeffrey <Jeffreym0215@gmail.com>
2020-01-24 15:12:23 -0800

    Coo Stuff

--- 1-Basic-HTML/sketch.js
+++ 1-Basic-HTML/sketch.js
@@ -1,12 +1,12 @@
-function setup() {
-  canvas = createCanvas(1440,1000);
-  canvas.position(0,0);
-  canvas.style('z-index','-1');
+// function setup() {
+//   canvas = createCanvas(windowWidth,windowHeight);
+//   canvas.position(0,0);
+//   canvas.style('z-index','-1');
+//
+//
+//   // put setup code here
+// }
 
-
-  // put setup code here
-}
-
-function draw() {
-  background('#F8B195');
-}
+// function draw() {
+//   background(Ombre.png);
+// }
